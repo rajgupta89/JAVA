097aaa1a1bd365df583caac5b8ea94b00c9166eb
rajgupta89 <gupta77raj123@gmail.com>
2021-12-09 12:28:42 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,8 @@
+public class string {
+    public static void main(String[] args) {
+        String product ="Pen";
+        product.toLowerCase();
+       product= product.concat("BOX".toLowerCase());
+        System.out.println(product.substring(0,4));
+    }
+}
