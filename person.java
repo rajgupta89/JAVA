424ee22561695d6f269fddeed85a757256d86b31
rajgupta89 <gupta77raj123@gmail.com>
2021-12-10 13:22:35 +0530

    commit

--- person.java
+++ person.java
@@ -1,22 +1,23 @@
 public class person {
     String name;
-   static int age=25;
-    public person(String name){
-        this(name, age);
-       // setName(name);
+    int age = 25;
+
+    public person(String name) {
+        this.name = name;
     }
-    public person(String name, int age) { 
-       // person(name, age);
-        //setAge(age);
-        this.name=name;
-        person.age=age;
+
+    public person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
-    public String show(){
+
+    public String show() {
         return name + " " + age;
     }
+
     public static void main(String[] args) {
-        person p1=new person("Jesse");
-        person p2=new person("Walter",52);
+        person p1 = new person("Jesse");
+        person p2 = new person("Walter", 52);
         System.out.println(p1.show());
         System.out.println(p2.show());
     }
