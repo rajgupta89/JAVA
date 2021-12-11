23c569ae4179c3af6922d2280350eebc01cabb8e
rajgupta89 <gupta77raj123@gmail.com>
2021-12-11 23:00:03 +0530

    commit

---
+++ pakage/accountbalance.java
@@ -0,0 +1,26 @@
+package pakage;
+// import pakage.balance;
+import java.util.Date;
+import java.util.*;
+import java.util.regex.*;
+
+public class accountbalance {
+    public static void main(String[] args) {
+        balance obj=new balance("Raj", 650.2);
+        obj.show();
+        System.out.println("Date :"+ new Date());
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter mobile no. :");
+        String mobile=sc.nextLine();
+        Pattern p=Pattern.compile("[0-9]{10}");
+        Matcher m=p.matcher(mobile);
+        if(m.matches())
+        {
+            System.out.println("valid no");
+        }
+        else
+        {
+            System.out.println("Invalid no");
+        }
+    }
+}
