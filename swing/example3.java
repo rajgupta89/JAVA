f1f3b9e5bb435d00b3384420de16e60a4fa8067b
rajgupta89 <gupta77raj123@gmail.com>
2021-12-22 17:30:34 +0530

    added check box

--- swing/example3.java
+++ swing/example3.java
@@ -11,19 +11,22 @@ public class example3 {
         JButton b3=new JButton("Button 3");
         JButton b4=new JButton("Button 4");
         JPanel p=new JPanel(new BorderLayout()); // new BorderLayout() function to include layout
-        JTextField t=new JTextField("Text");
+        // JTextField t=new JTextField("Text");
+        JCheckBox c=new JCheckBox();
         // b.setBounds(40,80,50,50);
         // t.setBounds(10,10,100,50);
-        p.add(t, BorderLayout.CENTER);
+        // p.add(t, BorderLayout.CENTER);
         p.add(b1, BorderLayout.EAST);   // layout direction 
         p.add(b2, BorderLayout.WEST);
         p.add(b3, BorderLayout.NORTH);
         p.add(b4, BorderLayout.SOUTH);
+        p.add(c);
         b1.setBackground(Color.red);    // give bgcolor to specific buttons
         b2.setBackground(Color.pink);
         b3.setBackground(Color.blue);
         b4.setBackground(Color.green);
-        t.setBackground(Color.yellow);
+        // t.setBackground(Color.yellow);
+        c.setBackground(Color.yellow);
         a.add(p);
         a.setSize(300,300);
         a.setVisible(true);
