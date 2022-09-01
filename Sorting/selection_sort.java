ad71e4f7a615b7b37dc555479837ed09dec42b77
rajgupta89 <gupta77raj123@gmail.com>
2022-09-01 12:38:23 +0530

    commit

--- Sorting/selection_sort.java
+++ Sorting/selection_sort.java
@@ -1,4 +1,4 @@
-public class bubble_sort {
+public class selection_sort {
     public void print(int[] a){
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]);
@@ -7,13 +7,15 @@ public class bubble_sort {
     public static void main(String[] args) {
          int[] a= {7,8,3,2,1};
          for (int i = 0; i < a.length-1; i++) {
-             for (int j = 0; j < a.length-1-i; j++) {
-                 if(a[j]>a[j+1]){
-                  int temp=a[j];
-                  a[j]=a[j+1];
-                  a[j+1]=temp;
+             int small=i;
+             for (int j = i+1; j < a.length; j++) {
+                 if(a[small]>a[j]){
+                     small=j;                     
                  }
              }
+             int temp=a[small];
+             a[small]=a[i];
+             a[i]=temp;
          }
          bubble_sort obj=new bubble_sort();
          obj.print(a);
